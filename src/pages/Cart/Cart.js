645b37e4a488556d8ee855caf32d299c53095331
방서빈 <newmso5382@gmail.com>
2023-02-17 06:11:58 +0900

    수정중

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -9,26 +9,6 @@ export default function Cart() {
   const params = useParams();
   const productId = params.id;
   const navigate = useNavigate();
-
-  const decreaseQuantity = () => {
-    if (count === 1) return;
-    setCount(prev => prev - 1);
-  };
-
-  const increaseQuantity = () => {
-    if (count >= 10) return;
-    setCount(prev => prev + 1);
-  };
-
-  const editQuantity = e => {
-    const switchedValueToNumber = Number(e.target.value);
-    if (switchedValueToNumber >= 11) {
-      alert('10보다클 수 없습니다');
-      return;
-    }
-    setCount(switchedValueToNumber);
-  };
-
   const submitCart = () => {
     fetch(`http://10.58.52.55:8000/cart`, {
       method: 'POST',
--- src/pages/Cart/CartList.js
+++ src/pages/Cart/CartList.js
@@ -6,19 +6,38 @@ const CartList = ({
   productPrice,
   productMainImage,
 }) => {
+  const decreaseQuantity = () => {
+    if (count === 1) return;
+    setCount(prev => prev - 1);
+  };
+
+  const increaseQuantity = () => {
+    if (count >= 10) return;
+    setCount(prev => prev + 1);
+  };
+
+  const editQuantity = e => {
+    const switchedValueToNumber = Number(e.target.value);
+    if (switchedValueToNumber >= 11) {
+      alert('10보다클 수 없습니다');
+      return;
+    }
+    setCount(switchedValueToNumber);
+  };
+
   return (
     <section className="cartProductList">
       <img width="200px" alt="productImg" src={productMainImage} />
       <p>{productName}</p>
       <p>{productPrice}</p>
-
-      {/* <input
-      // type="checkbox"
-      // onChange={e => {
-      //   handleCheckList(e.currentTa.checked, cart.id);
-      // }}
-      // checked={checkLists.includes(cart.id) ? true : false}
-      /> */}
+      <div className="quantity">
+        <p>구매수량</p>
+        <div className="quantityBox">
+          <button onClick={decreaseQuantity}>-</button>
+          <input onChange={editQuantity} value={count} />
+          <button onClick={increaseQuantity}>+</button>
+        </div>
+      </div>
     </section>
   );
 };
